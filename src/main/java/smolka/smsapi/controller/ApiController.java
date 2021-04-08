46abf6de0eca71c42c1eb011ba0c88754613bfff
ihar.smolka <ihar_smolka@senla.eu>
2021-04-08 18:48:41 +0300

    все еще подготовка

--- src/main/java/smolka/smsapi/controller/ApiController.java
+++ src/main/java/smolka/smsapi/controller/ApiController.java
@@ -2,10 +2,8 @@ package smolka.smsapi.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import smolka.smsapi.dto.ServiceMessage;
 import smolka.smsapi.exception.*;
 import smolka.smsapi.service.request_handler.RequestHandler;
@@ -17,6 +15,11 @@ public class ApiController {
 
     @Autowired
     private RequestHandler requestHandler;
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("I'm OK");
+    }
 
     @PostMapping
     public ServiceMessage<?> postProceed(@RequestBody String requestBody) throws UserNotFoundException, UserBalanceIsEmptyException, ActivationNotFoundException, ReceiverException, NoNumbersException {
